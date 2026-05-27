5e53bb27f30e505ffbf0ff39e9c358a9f582676d
Cyber-Knose <maxinfo989@gmail.com>
2026-05-27 14:49:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var originalImageData = null;
+var originalFileName = "image.jpg";
+var cleanImageData = null;
+var originalFileBytes = null;
+
+var imageInput = document.getElementById('imageInput');
+var imagePreview = document.getElementById('imagePreview');
+ rest of project in one file named index.html
